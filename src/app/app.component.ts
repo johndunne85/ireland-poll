4edeacb0bcbca311930df32bb70a4189860fd6e2
john dunne <johnjdunneone@yahoo.ie>
2018-05-10 23:34:09 +0100

    first commit transfered files for old ireland_poll to new one, ng new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+    showSideNav: boolean = false;
+
+    mediaItems = [
+      {name:"Dublin City", photo: "dublin.jpg"},
+      {name:"Limerick City", photo: "limerick.jpg"},
+      {name:"Donegal", photo: "donegal.jpg"},
+      {name:"Trinity College", photo: "trinity.jpg"},
+      {name:"Maynooth University", photo: "maynooth.jpg"}
+    ];
+
+      openNav(){
+          this.showSideNav = true;
+      }
+
+      closeNav(){
+        this.showSideNav = false;
+      }
 }
