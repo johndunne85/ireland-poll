24e8069a9afccadedf0174b7444499c9b7402d65
john dunne <johnjdunneone@yahoo.ie>
2018-06-19 21:08:34 +0100

    validation to form

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,  FormBuilder } from '@angular/forms';
+import { FormGroup,  FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { Customer } from './customer';
+
+function emailMatcher(c: AbstractControl){
+  let emailControl = c.get('email');
+  let confirmControl = c.get('confirmEmail');
+
+  if(emailControl.pristine || confirmControl.pristine){
+    return null;
+  }
+
+  if(emailControl.value === confirmControl.value){
+    return null;
+  }
+  return { 'match': true };
+}
+
+function ratingRange( c: AbstractControl): {[key: string]: boolean | null {
+  if(c.value != undefined && (isNaN(c.value) || c.value < 1 || c.value > 5)){
+      return {'range': true};
+  };
+  return null;
+};
+
+
+
 
 @Component({
   selector: 'app-signup',
@@ -15,9 +39,16 @@ export class SignupComponent implements OnInit {
 
     ngOnInit(): void{
       this.customerForm = this.fb.group({
-        firstName: '',
-        lastName: '',
-        email: '',
+        firstName: ['', [Validators.required, Validators.minLength(3)]],
+        lastName: ['',[Validators.required, Validators.maxLength(25)]],
+        emailGroup: this.fb.group({
+            email: ['',[Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+')]],
+            confirmEmail: ['', Validators.required],
+        }, {validator: emailMatcher});
+
+        phone:'',
+        notifications: 'email',
+        rating:['', ratingRange],
         sendCatalog: true
       });
     }
@@ -33,6 +64,16 @@ export class SignupComponent implements OnInit {
     save(){
       console.log(this.customerForm);
       console.log('Saved: '+JSON.stringify(this.customerForm.value));
+    }
+
+    setNotification(notifyVia: string): void {
+      const phoneControl = this.customerForm.get('phone');
+      if(notifyVia === 'text'){
+        phoneControl.setValidators(Validators.required);
+      }else{
+        phoneControl.clearValidators();
+      }
+      phoneControl.updateValueAndValidity();
     }
 
 
